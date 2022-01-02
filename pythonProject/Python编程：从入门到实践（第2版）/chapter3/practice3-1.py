905267a8199f27ca46d6a2abd0b6cebfbf55ecbc
KDH <995533366@qq.com>
2022-01-02 21:06:25 +0800

    20220102

--- pythonProject/Python编程：从入门到实践（第2版）/chapter3/practice3-1.py
+++ pythonProject/Python编程：从入门到实践（第2版）/chapter3/practice3-1.py
@@ -1,17 +1,17 @@
 # Author：D.H
 # Place：哈工深
 # Time：2021/7/24 15:48
-#练习3-1：姓名
+# 练习3-1：姓名
 
-names=['lucy,jack,xiaoming,xiaohong']
+names = ['lucy,jack,xiaoming,xiaohong']
 print(names[0])
 
-#依次输出名字的正确写法
-names=['lucy','jack','xiaoming','xiaohong']
-print(names[0]) #索引从0开始，代表第1个位置
+# 依次输出名字的正确写法
+names = ['lucy', 'jack', 'xiaoming', 'xiaohong']
+print(names[0])  # 索引从0开始，代表第1个位置
 print(names[1])
 print(names[2])
 print(names[3])
 print('----------')
-print(names[-1]) #可通过负数倒推索引
+print(names[-1])  # 可通过负数倒推索引
 print(names[-2])
